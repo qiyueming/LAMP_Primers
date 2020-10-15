d25a0458520e1908682fd9865d2a619e8fcb484e
Hui Kang <rocksnow1942@gmail.com>
2020-10-15 10:53:38 -0700

    ON: Thu Oct 15 10:53:38 2020, FROM: mbp

---
+++ Flu_primer_design/design_thoughts.py
@@ -0,0 +1,33 @@
+#FluA M gene in NorthAmerica Human Host, all years serotypes:
+{'H1N1': 12917,
+'H3N2': 17582,
+'H1': 36,
+'H3': 101,
+'H2N2': 53,
+'mixed': 19,
+'N2': 8,
+'unknown': 4,
+'H1N2v': 6,
+None: 32,
+'H7N3': 2,
+'H3N2v': 1,
+'N1': 6,
+'H1N2': 25,
+'H7N2': 1}
+
+
+# 
+# fluA H1N1 M gene aligned 
+# fluA H3N2 M gene also aligned
+# both H1N1 and H2N2 are aligned together 
+# compare the alignment.
+
+
+
+"""
+# TODO:
+since the flu genes are much more variable than regular, need to use degenerate sequences as target.
+
+
+
+"""
